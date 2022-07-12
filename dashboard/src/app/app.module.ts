3e6b946b52336f4a4791cd0227428f6338ae596b
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2022-07-12 10:32:00 +0100

    starting main dashboard page

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { DefaultModule } from './default/default.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]

a6e853725841931fbaa79dd72e17609a95aa9958
William-Edwards <67965211+William-Edwards@users.noreply.github.com>
2022-08-02 14:07:23 +0100

    backend working
    starting changes on frontend to reflect changes to schema

--- dashboard/src/app/auth/auth.service.ts
+++ dashboard/src/app/auth/auth.service.ts
@@ -3,44 +3,52 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs';
 
-import { User } from '../users/users';
+import { Account, User } from '../models/account';
 import { ThisReceiver, Token } from '@angular/compiler';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private currentUserSubject: BehaviorSubject<User>;
-  public currentUser: Observable<User>;
+  private accountSubject: BehaviorSubject<Account>;
+  public account: Observable<Account>;
   private API_URL = environment.apiUrl
 
-  constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
-    this.currentUser = this.currentUserSubject.asObservable();
+  constructor(
+    private router: Router,
+    private http: HttpClient) {
+    this.accountSubject = new BehaviorSubject<Account>(null);
+    this.account = this.accountSubject.asObservable();
 
   }
 
-  public get currentUserValue(): User {
-    return this.currentUserSubject.value;
+  public get accountValue(): Account {
+    return this.accountSubject.value;
   }
 
-  login(username, password) {
-    return this.http.post<any>(this.API_URL + '/users/authenticate', { username, password })
-      .pipe(map(user => {
-
-        localStorage.setItem('currentUser', JSON.stringify(user));
-        this.currentUserSubject.next(user);
-        return user;
+  login(email: string, password: string) {
+    return this.http.post<any>(this.API_URL + '/accounts/authenticate', { email, password }, { withCredentials: true })
+      .pipe(map(account => {
+        this.accountSubject.next(account);
+        this.startRefreshTokenTimer();
+        return account;
       }));
 
   }
 
   logout() {
-
-    localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null)
+    this.http.post<any>(this.API_URL + '/accounts/revoke-token', {}, { withCredentials: true }).subscribe();
+    this.stopRefreshTokenTimer();
+    this.accountSubject.next(null);
+    this.router.navigate(['/auth/login']);
   }
+
+  refreshToken() {
+    this.http.post<any>
+  }
+
 
   register(user: User) {
     return this.http.post(this.API_URL + '/users/register', user);
